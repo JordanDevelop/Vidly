aef753c3d58e12c100a946e07b4db154b5f442cf
JordanDevelop <jordan@jordandevelop.com>
2015-09-15 11:22:30 -0700

    removed cdn variable from outputs base_url

--- modules/articles/server/routes/articles.server.routes.js
+++ modules/articles/server/routes/articles.server.routes.js
@@ -59,19 +59,19 @@ config.zencoder = {
     outputs: function(id) { // Eventually we may want to pass things to our outputs array...
         var outputs = [{
             label: 'MP4',
-            base_url: config.zencoder.cdn,
+            base_url: config.zencoder.output_url,
             public: true,
             thumbnails: {
                 width: 375,
                 height: 220,
                 number: 1,
-                base_url: config.zencoder.cdn,
+                base_url: config.zencoder.output_url,
                 filename: '{{number}}_' + id,
                 public: true
             }
         }, {
             label: 'WebM',
-            base_url: config.zencoder.cdn,
+            base_url: config.zencoder.output_url,
             format: 'webm',
             public: true
         }]
