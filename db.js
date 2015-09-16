10e3abe43d0e8e271c110e7deb56b8f41be2d862
meghathareja <mss.meghathareja@gmail.com>
2015-09-16 15:06:30 +0530

    No conflicts

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 // config/database.js
  module.exports = {
      'connection': {
-        'host': '10.0.10.193',
-        'user': 'VV_JyIThFn',
- 		'password': 'KU6caDkvZ7B74#'
- 		// 'host': 'localhost',
-   //      'user': 'root',
- 		// 'password': 'root'
+   //      'host': '10.0.10.193',
+   //      'user': 'VV_JyIThFn',
+ 		// 'password': 'KU6caDkvZ7B74#'
+ 		'host': 'localhost',
+        'user': 'root',
+ 		'password': 'root'
     },
-     'database': 'vidlydb713',
-    //'database': 'vidly',
+    // 'database': 'vidlydb713',
+    'database': 'vidly',
     'port':61337
  };
  
