fe1408dfcbd9b363edcd157f09f7c9529e061dc3
Jordan Simpson <jordandirectly@gmail.com>
2016-11-30 14:52:55 -0800

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 var config = {}
 
 // INK Filepicker API key
-config.filepicker = 'Av4QSKNOQSObS35rGlB8Bz';
+config.filepicker = 'FILL';
 
 // Zencoder specific configuration
 config.zencoder = {
-  api_key: 'a2216d9259ff3f0e387bde6047c06a87', // API key
+  api_key: 'FILL', // API key
 
   output_url: 's3://vidly-bucket/', // Output location for your transcoded videos
   cdn: 'https://c.vidly.io/', // CDN URL
